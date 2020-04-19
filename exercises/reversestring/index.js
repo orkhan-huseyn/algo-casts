c99ad95822bdce03b305a2b0caf0d43a15f37168
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-04-19 15:09:32 +0400

    initial commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reversedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    reversedStr = str[i] + reversedStr;
+  }
+  return reversedStr;
+}
 
 module.exports = reverse;
